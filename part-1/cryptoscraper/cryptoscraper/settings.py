0fe050158bbad698c35d1144279ce0a69723c58e
rahibvk <91156242+rahibvk@users.noreply.github.com>
2024-09-15 22:57:49 +0300

    updated code

--- part-1/cryptoscraper/cryptoscraper/settings.py
+++ part-1/cryptoscraper/cryptoscraper/settings.py
@@ -12,80 +12,72 @@ BOT_NAME = "cryptoscraper"
 SPIDER_MODULES = ["cryptoscraper.spiders"]
 NEWSPIDER_MODULE = "cryptoscraper.spiders"
 
-
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = "cryptoscraper (+http://www.yourdomain.com)"
+# USER_AGENT = "cryptoscraper (+http://www.yourdomain.com)"
 
-# Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+# Obey robots.txt rules (disable to crawl all links)
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
-# See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
-# See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
-# The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
+DOWNLOAD_DELAY = 1  # Adjust this based on server load
 
-# Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+# Enable AutoThrottle to manage request rate automatically
+AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_START_DELAY = 2  # Delay before requests are throttled
+AUTOTHROTTLE_MAX_DELAY = 10  # Max delay in case of high latencies
+AUTOTHROTTLE_TARGET_CONCURRENCY = 3.0  # Average number of requests Scrapy should be sending in parallel
+AUTOTHROTTLE_DEBUG = False  # Set to True to see throttling stats in logs
+
+# Enable cookies (disabled by default)
+# COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
+# TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
+# DEFAULT_REQUEST_HEADERS = {
 #    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
 #    "Accept-Language": "en",
-#}
+# }
 
 # Enable or disable spider middlewares
-# See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
+# SPIDER_MIDDLEWARES = {
 #    "cryptoscraper.middlewares.CryptoscraperSpiderMiddleware": 543,
-#}
+# }
 
 # Enable or disable downloader middlewares
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
+# DOWNLOADER_MIDDLEWARES = {
 #    "cryptoscraper.middlewares.CryptoscraperDownloaderMiddleware": 543,
-#}
+# }
 
 # Enable or disable extensions
-# See https://docs.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
+# EXTENSIONS = {
 #    "scrapy.extensions.telnet.TelnetConsole": None,
-#}
+# }
 
 # Configure item pipelines
-# See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
+# ITEM_PIPELINES = {
 #    "cryptoscraper.pipelines.CryptoscraperPipeline": 300,
-#}
+# }
 
-# Enable and configure the AutoThrottle extension (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
-# The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
-# The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
-# The average number of requests Scrapy should be sending in parallel to
-# each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
-# Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
+# Set download timeout to avoid hanging requests
+DOWNLOAD_TIMEOUT = 180  # 3 minutes
+
+# Handle 302 redirects
+REDIRECT_ENABLED = True
 
 # Enable and configure HTTP caching (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
-#HTTPCACHE_DIR = "httpcache"
-#HTTPCACHE_IGNORE_HTTP_CODES = []
-#HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
+HTTPCACHE_ENABLED = True
+HTTPCACHE_EXPIRATION_SECS = 0  # No expiration for cache
+HTTPCACHE_DIR = "httpcache"
+HTTPCACHE_IGNORE_HTTP_CODES = []
+HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
+
+# Prevent filtering out duplicate URLs
+DUPEFILTER_CLASS = 'scrapy.dupefilters.RFPDupeFilter'
 
 # Set settings whose default value is deprecated to a future-proof value
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
--- part-1/cryptoscraper/cryptoscraper/spiders/bookspider.py
+++ part-1/cryptoscraper/cryptoscraper/spiders/bookspider.py
@@ -1,20 +1,44 @@
 import scrapy
-
+import logging
 
 class BookspiderSpider(scrapy.Spider):
     name = "bookspider"
     allowed_domains = ["bscscan.com"]
-    start_urls = ["https://bscscan.com/accounts"]
+    start_urls = ["https://bscscan.com/accounts/1"]  # Start with page 1
 
     custom_settings = {
         'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
         'ROBOTSTXT_OBEY': False,
         'REDIRECT_ENABLED': True,
-        'REDIRECT_MAX_TIMES': 5
+        'REDIRECT_MAX_TIMES': 5,
+        'DUPEFILTER_DEBUG': True,
+        'FEED_FORMAT': 'json',
+        'FEED_URI': 'output.json',  # Save the results to 'output.json'
+        'LOG_LEVEL': 'DEBUG',  # Set logging level to DEBUG
     }
 
     def parse(self, response):
+        # Extract all account links
         links = response.css('a.me-1::attr(href)').getall()
-        
+        logging.info(f"Scraped {len(links)} links from {response.url}")
+
         for link in links:
+            # Yield the account links
             yield {'link': link}
+
+        # Extract the current page number from the URL
+        current_page = response.url.split('/')[-1]
+        if current_page.isdigit():
+            current_page = int(current_page)
+        else:
+            current_page = 1
+
+        # Generate the next page URL
+        next_page = current_page + 1
+        if next_page <= 400:  # Adjust this if there are more or fewer pages
+            next_page_url = response.urljoin(f"/accounts/{next_page}")
+            logging.info(f"Found next page: {next_page_url}")
+            # Follow the next page link and call the parse function recursively
+            yield scrapy.Request(next_page_url, callback=self.parse, dont_filter=True)
+        else:
+            logging.info("No more pages to scrape.")
